d464bf8710f5e6f78ef46fcacd266e33acde335f
nrajanithin <nrajanithin@gmail.com>
2021-03-24 23:48:28 -0400

    stage

--- app.js
+++ app.js
@@ -25,7 +25,98 @@ app.use(cors())
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.get('/',(req,res)=>{
-    res.send("hi mama");
+  const sql = `select sum(spend) as ss,store_r from tr, pr where pr.PRODUCT_NUM=tr.PRODUCT_NUM and pr.DEPARTMENT='PHARMA' group
+  by store_r;`
+  con.query(sql, function (err, result) {
+    if (err) 
+    {
+      throw err;
+    }
+    console.log("Result: " + result);
+    res.send(result);
+  });
+  
+})
+app.get('/food',(req,res)=>{
+  const sql = `select sum(spend) as ss,store_r from tr, pr where pr.PRODUCT_NUM=tr.PRODUCT_NUM and pr.DEPARTMENT='FOOD' group
+  by store_r;`
+  con.query(sql, function (err, result) {
+    if (err) 
+    {
+      throw err;
+    }
+    console.log("Result: " + result);
+    res.send(result);
+  });
+  
+})
+app.get('/nonfood',(req,res)=>{
+  const sql = `select sum(spend) as ss,store_r from tr, pr where pr.PRODUCT_NUM=tr.PRODUCT_NUM and pr.DEPARTMENT='NON-FOOD' group
+  by store_r;`
+  con.query(sql, function (err, result) {
+    if (err) 
+    {
+      throw err;
+    }
+    console.log("Result: " + result);
+    res.send(result);
+  });
+  
+})
+app.get('/line',(req,res)=>{
+  const sql = `select sum(SPEND) as ss,store_r, 'JAN' as Month from tr where PURCHASE_ like "%JAN%" group by store_r union 
+  select sum(SPEND) as ss,store_r, 'FEB' as Month from tr where PURCHASE_ like "%FEB%" group by store_r union 
+  select sum(SPEND) as ss,store_r, 'MAR' as Month from tr where PURCHASE_ like "%MAR%" group by store_r union 
+  select sum(SPEND) as ss,store_r, 'APR' as Month from tr where PURCHASE_ like "%APR%" group by store_r union
+  select sum(SPEND) as ss,store_r, 'MAY' as Month from tr where PURCHASE_ like "%MAY%" group by store_r union 
+  select sum(SPEND) as ss,store_r, 'JUN' as Month from tr where PURCHASE_ like "%JUN%" group by store_r union 
+  select sum(SPEND) as ss,store_r, 'JUL' as Month from tr where PURCHASE_ like "%JUL%" group by store_r union 
+  select sum(SPEND) as ss,store_r, 'AUG' as Month from tr where PURCHASE_ like "%AUG%" group by store_r union 
+  select sum(SPEND) as ss,store_r, 'SEP' as Month from tr where PURCHASE_ like "%SEP%" group by store_r union 
+  select sum(SPEND) as ss,store_r, 'OCT' as Month from tr where PURCHASE_ like "%OCT%" group by store_r union 
+  select sum(SPEND) as ss,store_r, 'NOV' as Month from tr where PURCHASE_ like "%NOV%" group by store_r union 
+  select sum(SPEND) as ss,store_r, 'DEC' as Month from tr where PURCHASE_ like "%DEC%" group by store_r`
+  con.query(sql, function (err, result) {
+    if (err) 
+    {
+      throw err;
+    }
+    console.log("Result: " + result);
+    res.send(result);
+  });
+})
+app.get('/don',(req,res)=>{
+  const sql = `select sum(spend) as ss,INCOME_RANGE from hh,tr where tr.HSHD_NUM = hh.HSHD_NUM group by INCOME_RANGE;`
+  con.query(sql, function (err, result) {
+    if (err) 
+    {
+      throw err;
+    }
+    console.log("Result: " + result);
+    res.send(result);
+  });
+})
+app.get('/bar',(req,res)=>{
+  const sql =`select sum(spend) as ss, department, "JAN" as MONTH from tr, pr where tr.PRODUCT_NUM=pr.PRODUCT_NUM and PURCHASE_ like '%JAN%' GROUP BY department union
+   select sum(spend) as ss, department, "FEB" as MONTH from tr, pr where tr.PRODUCT_NUM=pr.PRODUCT_NUM and PURCHASE_ like '%FEB%' GROUP BY department union 
+   select sum(spend) as ss, department, "MAR" as MONTH from tr, pr where tr.PRODUCT_NUM=pr.PRODUCT_NUM and PURCHASE_ like '%MAR%' GROUP BY department union 
+   select sum(spend) as ss, department, "APR" as MONTH from tr, pr where tr.PRODUCT_NUM=pr.PRODUCT_NUM and PURCHASE_ like '%APR%' GROUP BY department union 
+   select sum(spend) as ss, department, "MAY" as MONTH from tr, pr where tr.PRODUCT_NUM=pr.PRODUCT_NUM and PURCHASE_ like '%MAY%' GROUP BY department union 
+   select sum(spend) as ss, department, "JUN" as MONTH from tr, pr where tr.PRODUCT_NUM=pr.PRODUCT_NUM and PURCHASE_ like '%JUN%' GROUP BY department union 
+   select sum(spend) as ss, department, "JUL" as MONTH from tr, pr where tr.PRODUCT_NUM=pr.PRODUCT_NUM and PURCHASE_ like '%JUL%' GROUP BY department union 
+   select sum(spend) as ss, department, "AUG" as MONTH from tr, pr where tr.PRODUCT_NUM=pr.PRODUCT_NUM and PURCHASE_ like '%AUG%' GROUP BY department union 
+   select sum(spend) as ss, department, "SEP" as MONTH from tr, pr where tr.PRODUCT_NUM=pr.PRODUCT_NUM and PURCHASE_ like '%SEP%' GROUP BY department union 
+   select sum(spend) as ss, department, "OCT" as MONTH from tr, pr where tr.PRODUCT_NUM=pr.PRODUCT_NUM and PURCHASE_ like '%OCT%' GROUP BY department union 
+   select sum(spend) as ss, department, "NOV" as MONTH from tr, pr where tr.PRODUCT_NUM=pr.PRODUCT_NUM and PURCHASE_ like '%NOV%' GROUP BY department union 
+   select sum(spend) as ss, department, "DEC" as MONTH from tr, pr where tr.PRODUCT_NUM=pr.PRODUCT_NUM and PURCHASE_ like '%DEC%' GROUP BY department`
+  con.query(sql, function (err, result) {
+    if (err) 
+    {
+      throw err;
+    }
+    console.log("Result: " + result);
+    res.send(result);
+  });
 })
 app.post('/data/hh',(req,res)=>{
    const x = req.body.data;
